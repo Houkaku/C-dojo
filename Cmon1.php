efb854156cee7902a178aee09f7c6200a58e6ebe
Houkaku <twforeverangel@gmail.com>
2016-09-13 14:52:00 +0900

    難易度　低　問題１

---
+++ Cmon1.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>問題１</title>
+</head>
+
+
+<body>
+<p>問題１</p>
+<p>３+５の計算をし、答えを表示するプログラムを作成しなさい。</p>
+
+<form>
+<p>回答</p>
+<form action="Cmon1compile.php" method="psot">
+<input type="example" cols="100" rows="30"><BR>
+<input type="submit" value="採点"><br>
+</form>
+<p>　　　　　　　　　　        　　　　　　　　　　　　　　　　<a href="http://localhost/menu.php">メニューへ</a> 　　　　　　　　　　　　　　<strong><a href="http://localhost/Cmon2.php">次の問題</a></strong></p>
+<p>　　　　　　送信　　        　　　　　　　　　　　　　　　　<a href="http://localhost/Cmon1hint.php" target="_blank">ヒント</a>　　     　　　　　　　　　　　　　　採点　　</p>
+</form>
+</body>
+</html>

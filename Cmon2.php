b439d78ef6cc2ec6e496dce172cbcf05b1a5908c
Houkaku <twforeverangel@gmail.com>
2016-09-14 09:37:39 +0900

    難易度　低　問題２

---
+++ Cmon2.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>問題２</title>
+</head>
+
+<body>
+<p>問題２</p>
+<p>ループを使用し、ｎが１０になるまで２を足し続けるプログラムを書きなさい。</p>
+
+<form>
+<p>回答</p>
+<textarea name="example" cols="100" rows="30"></textarea>
+<p>　　　　　　<a href="http://localhost/Cmon1.php">前の問題</a> 　　　　　　　　　　　　　　　　<a href="http://localhost/menu.php">メニューへ</a> 　　　　　　　　　　　　　　<a href="http://localhost/Cmon3.php">次の問題</a></p>
+<p>　　　　　　　送信　　  　      　　　　　　　　　　　　　　　　<a href="http://localhost/Cmon2hint.php">ヒント</a>　　     　　　　　　　　　　　　　　　採点　　</p>
+</form>
+</body>
+</html>

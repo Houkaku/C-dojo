e282bc9f39ddc7ac15884e3a155fb205cb105daf
Houkaku <twforeverangel@gmail.com>
2016-09-14 09:36:36 +0900

    難易度　低　問題１

---
+++ Cmon1compile.php
@@ -0,0 +1,9 @@
+<?php
+
+//system('set PATH=Z:\ƒvƒƒOƒ‰ƒ~ƒ“ƒO‹Zp\borland\bcc55\Bin;C:\ProgramData\Oracle\Java\javapath;C:\PROGRA~1\COMMON~1\MICROS~1\WINDOW~1;C:\PROGRA~2\COMMON~1\MICROS~1\WINDOW~1;C:\Windows\System32;C:\Windows;C:\Windows\System32\wbem;C:\PROGRA~2\WIC4A1~1\Shared;C:\PROGRA~1\Intel\INTEL(~1\DAL;C:\PROGRA~1\Intel\INTEL(~1\IPT;C:\PROGRA~2\Intel\INTEL(~1\DAL;C:\PROGRA~2\Intel\INTEL(~1\IPT');
+//system('cd ƒvƒƒOƒ‰ƒ~ƒ“ƒO‹Zp');
+//system('bcc32 E-21.c');
+
+
+system('set PATH=Z:\ƒvƒƒOƒ‰ƒ~ƒ“ƒO‹Zp\borland\bcc55\Bin;C:\ProgramData\Oracle\Java\javapath;C:\PROGRA~1\COMMON~1\MICROS~1\WINDOW~1;C:\PROGRA~2\COMMON~1\MICROS~1\WINDOW~1;C:\Windows\System32;C:\Windows;C:\Windows\System32\wbem;C:\PROGRA~2\WIC4A1~1\Shared;C:\PROGRA~1\Intel\INTEL(~1\DAL;C:\PROGRA~1\Intel\INTEL(~1\IPT;C:\PROGRA~2\Intel\INTEL(~1\DAL;C:\PROGRA~2\Intel\INTEL(~1\IPT & cd /d Z:\ƒvƒƒOƒ‰ƒ~ƒ“ƒO‹Zp & bcc32 E2-24.c');
+?>
---
+++ Out.php
@@ -0,0 +1,4 @@
+<form action="ファイル名" method="post">
+ <input type="text" name="data1" size="10">
+ <input type="submit" value="送信">
+ </form>

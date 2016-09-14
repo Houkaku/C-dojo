e598f30df771a2d0e460675c0e1c0f186a6838ef
Houkaku <twforeverangel@gmail.com>
2016-09-14 09:38:38 +0900

    メニュー画面

---
+++ menu.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<html lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=Shift_JIS">
+<meta http-equiv="Content-Style-Type" content="text/css">
+<meta http-equiv="Content-Script-Type" content="text/javascript">
+
+<title>メニュー</title>
+
+<script type="text/javascript" language="JavaScript" src="../../js/roller.js"></script>
+<script type="text/javascript" language="JavaScript" src="../../js/jump1.js"></script>
+</head>
+
+<body>
+<p>　　　　　　　　　　　　　　　　　　　　難易度</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>　　　<a href="http://localhost/Cmon1.php">低</a>　　　　　　　　　　　　　　　　　中　　　　　　　　　　　　　　　　　　　　高</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>ログアウト　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　過去の成績</p>
+</body>
+</html>

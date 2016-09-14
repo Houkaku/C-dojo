3fd62d1a34aacb4afbac406534cc1f4a0125a094
Houkaku <twforeverangel@gmail.com>
2016-09-14 09:37:05 +0900

    難易度　低　問題１ ヒント

---
+++ Cmon1hint.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>問題１ヒント</title>
+</head>
+<body>
+<p>問題１ヒント</p>
+
+<p>変数を３つ宣言し、１つに３、1つに５を代入、もう一つの変数に左辺値として計算を代入する。</p>
+</body>
+</html>

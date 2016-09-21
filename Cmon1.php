5b72f7261c3b4bbf4448918d8e2ab2a9c86e8771
Houkaku <twforeverangel@gmail.com>
2016-09-21 11:01:04 +0900

    コンパイルのページとの連携に成功

---
+++ Cmon1.php
@@ -0,0 +1,22 @@
+﻿
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>問題１</title>
+</head>
+
+
+<body>
+<p>問題１</p>
+<p>３+５の計算をし、答えを表示するプログラムを作成しなさい。</p>
+
+<form action="Cmon1compile.php" method="post">
+<p>回答</p>
+<textarea name="answer" cols="80" rows="15"></textarea>
+<input type="hidden" name="mon" value="1">
+
+<p>　　　　　　　　　　        　　　　　　　　　　　　　　　　<a href="http://localhost/menu.php">メニューへ</a> 　　　　　　　　　　　　　　<strong><a href="http://localhost/Cmon2.php">次の問題</a></strong></p>
+<p>　　　　　　<input type="submit" value="送信">　        　　　　　　　　　　　　　　　　<a href="http://localhost/Cmon1hint.php" target="_blank">ヒント</a>　　     　　　　　　　　　　　　　　採点　　</p>
+</form>
+</body>
+</html>

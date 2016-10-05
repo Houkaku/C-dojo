768445857873d6c5758ba1a72fbaa8ac3705fc23
kudotimu <kudotiko999@ezweb.ne.jp>
2016-10-05 09:50:19 +0900

    メニュー画面 (csssample2.php)

---
+++ csssample2.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<html lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=Shift_JIS">
+<meta http-equiv="Content-Style-Type" content="text/css">
+<meta http-equiv="Content-Script-Type" content="text/javascript">
+
+<title>メニュー</title>
+
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap-material-design.css">
+<link rel="stylesheet" type="text/css" href="css/ripples.css">
+<link rel="stylesheet" type="text/css" href="css/styles.css">
+
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/material.min.js"></script>
+<script type="text/javascript" src="js/ripples.min.js"></script>
+
+
+</head>
+
+<body>
+<h1>難易度</h1>
+<div class="btn-group btn-group-raised difficulty">
+    <a class="btn btn-info" href="Cmon1.php">低</a>
+    <a class="btn btn-success" href="#">中</a>
+    <a class="btn btn-warning" href="#">高</a>
+</div>
+
+<table>
+    <tr>
+        <td><a class="btn btn-raised btn-warning" href="login.php">ログアウト</a></td>
+    </tr>
+</table>
+<!--
+<p><a href="login.php">ログアウト</a>
+過去の成績</p>
+-->
+
+</body>
+</html>

938b64f9e990819f9112df3d1ba43b48b0349ee6
kudotimu <kudotiko999@ezweb.ne.jp>
2016-11-02 10:23:23 +0900

    hai

--- Cmon1.php
+++ Cmon1.php
@@ -1,8 +1,7 @@
-﻿
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>問題１</title>
 
 <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="css/bootstrap-material-design.css">
@@ -13,44 +12,70 @@
 <script type="text/javascript" src="js/bootstrap.min.js"></script>
 <script type="text/javascript" src="js/material.min.js"></script>
 <script type="text/javascript" src="js/ripples.min.js"></script>
+
+<style>
+
+table{
+margin-bottom: 10px;
+border-collapse: separate;
+}
+
+table.center{
+border-spacing: 40px 10px;
+text-aling:center;
+}
+
+.center1{
+    font-size:200%;
+    text-align: center;
+    line-height:100px;
+}
+
+.center2{
+    text-align: center;
+}
+
+body{ 
+  background-color:#dddddd;
+}
+
+</style>
+
+<title>問題１</title>
 </head>
 
 <body>
-
-<p>問題１</p>
-
-<?php
-if(isset($_COOKIE['a'])){
-	$getval = $_COOKIE['a'];
-	echo $getval;
-}
-?>
-
-<p>３+５の計算をし、答えを表示するプログラムを作成しなさい。</p>
-<form action="Cmon1compile.php" method="post">
-<p>回答</p>
-<textarea name="answer" cols="80" rows="15"></textarea>
-<input type="hidden" name="mon" value="1">
-<p>
-<div class="btn-group">
-	　　　　　　　　　　        　　　　　　　　　　　　　　　　
-    
-	<p>　　　　　　　　　　　　　　　　　　　　　
-	<a class="btn btn-info" href="menu.php">メニュー</a>　　　　　　　　　　　　　　
-	<a class="btn btn-info" href="Cmon2.php">次の問題</a>
-      
-	</p>
-    
-	</p>
-    
-	<p>
-      
-	<button type="submit" class="btn btn-info"> 送 信</button>　        　　　　　　　　　　　　　　  
-	<a class="btn btn-info" href="Cmon1hint.php" target="_blank">ヒ ン ト</a>
-　　     　　　　　　　　　　　　　　
-    <a class="btn btn-info" href="saitenn.php">採点</a>　　</p>
+<div class="center1">
+    問題１<br>
+    ３+５の計算をし、答えを表示するプログラムを作成しなさい。
 </div>
 
+<div class="center2">
+<form class="form-horizontal">
+    <textarea name="answer" cols="80" rows="30">
+#include <stdio.h>
+void main(void) {
+   //ここにソースを記述する
+}
+    </textarea>
+    <input type="hidden" name="mon" value="1">
+<p>
 </form>
+<div class="btn-group">
+    <table class="center">
+        <tr>
+            <td><a class="btn btn-info" href="aaa.php"><font size="6">メニューへ</a></td>
+            <td><a class="btn btn-info" href="Cmon2.php"><font size="6">次の問題</a></td>
+        </tr>
+    </table>
+    <table class="center">
+        <tr>
+            <td><a class="btn btn-info" href="#"><font size="6">送信</a></td>
+            <td><a class="btn btn-info" href="Cmon1hint.php" target="_blank"><font size="6">ヒント</a></td>
+            <td><a class="btn btn-info" href="#"><font size="6">採点</td>
+        </tr>
+    </table>
+    </div>
+</div>
 </body>
 </html>

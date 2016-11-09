c7403ef1b715bcbe81293dafa56110fb5e67c5f2
Houkaku <twforeverangel@gmail.com>
2016-11-09 12:04:30 +0900

    aa

--- Cmon1desin.php
+++ Cmon1desin.php
@@ -1,49 +1,4 @@
-<<<<<<< HEAD
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
-<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-<link rel="stylesheet" type="text/css" href="css/bootstrap-material-design.css">
-<link rel="stylesheet" type="text/css" href="css/ripples.css">
-<link rel="stylesheet" type="text/css" href="css/styles.css">
-
-<script type="text/javascript" src="js/jquery.min.js"></script>
-<script type="text/javascript" src="js/bootstrap.min.js"></script>
-<script type="text/javascript" src="js/material.min.js"></script>
-<script type="text/javascript" src="js/ripples.min.js"></script>
-
-<style>
-
-table{
-margin-bottom: 10px;
-border-collapse: separate;
-}
-
-table.center{
-border-spacing: 40px 10px;
-text-aling:center;
-}
-
-.center1{
-    font-size:200%;
-    text-align: center;
-    line-height:100px;
-}
-
-.center2{
-    text-align: center;
-}
-
-body{ 
-  background-color:#dddddd;
-}
-
-</style>
-
-<title>問題１</title>
-</head>
+include("Cmonheaddesin");
 
 <body>
 <div class="center1">
@@ -79,24 +34,4 @@ void main(void) {
     </div>
 </div>
 =======
-﻿<html>
-
-<head>
-<?php
-$title = '問題1';
-$monNum = 1;
-include( "Cmonhead.php" );
-?>
-</head>
-
-<body>
-<?php include("body.php"); ?>
-<?php
-require("Cmon.php");
-echo mon1;
-?>
-<?php include("form.php"); ?>
->>>>>>> dd7ce11c86cd073cdc7a9e20720dd21e42589137
-</body>
-
-</html>
+?<html>
--- form.php
+++ form.php
@@ -1,26 +1,6 @@
-<p></p>
-<form action="Cmon<?php echo $monNum; ?>compile.php" method="post">
-<input type="hidden" name="mon" value="<?php echo $monNum; ?>">
-<p>回答</p>
-<textarea name="answer" cols="80" rows="15"></textarea>
-<p>
-<div class="btn-group">
-	　　　　　　　　　　        　　　　　　　　　　　　　　　　
-    
-	<p>　　　　　　　　　　　　　　　　　　　　　
-	<a class="btn btn-info" href="menu.php">メニュー</a>　　　　　　　　　　　　　　
-	<a class="btn btn-info" href="Cmon2.php">次の問題</a>
-      
-	</p>
-    
-
-    
-	<p>
-      
-	<button type="submit" class="btn btn-info"> 送 信</button>　        　　　　　　　　　　　　　　  
-	<a class="btn btn-info" href="Cmon1hint.php" target="_blank">ヒ ン ト</a>
-　　     　　　　　　　　　　　　　　
-    <a class="btn btn-info" href="saitenn.php">採点</a>　　</p>
-</div>
-
-</form>
+<?php
+$title = '問題1';
+$monText ="３+５の計算をし、答えを表示するプログラムを作成しなさい。";
+$monNum = 1;
+include ("Cmondesin");
+?>

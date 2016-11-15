6dd24bc8ce1175d81c9acba4d2f9c4e6a1fab01c
Houkaku <twforeverangel@gmail.com>
2016-11-15 13:40:30 +0900

    短縮済み

--- Cmon1desin.php
+++ Cmon1desin.php
@@ -1,37 +1,6 @@
-include("Cmonheaddesin");
-
-<body>
-<div class="center1">
-    問題１<br>
-    ３+５の計算をし、答えを表示するプログラムを作成しなさい。
-</div>
-
-<div class="center2">
-<form class="form-horizontal">
-    <textarea name="answer" cols="80" rows="30">
-#include <stdio.h>
-void main(void) {
-   //ここにソースを記述する
-}
-    </textarea>
-    <input type="hidden" name="mon" value="1">
-<p>
-</form>
-<div class="btn-group">
-    <table class="center">
-        <tr>
-            <td><a class="btn btn-info" href="aaa.php"><font size="6">メニューへ</a></td>
-            <td><a class="btn btn-info" href="Cmon2.php"><font size="6">次の問題</a></td>
-        </tr>
-    </table>
-    <table class="center">
-        <tr>
-            <td><a class="btn btn-info" href="#"><font size="6">送信</a></td>
-            <td><a class="btn btn-info" href="Cmon1hint.php" target="_blank"><font size="6">ヒント</a></td>
-            <td><a class="btn btn-info" href="#"><font size="6">採点</td>
-        </tr>
-    </table>
-    </div>
-</div>
-=======
-?<html>
+<?php
+$title = '問題1';
+$monText = "３+５の計算をし、答えを表示するプログラムを作成しなさい。";
+$monNum = 1;
+include( "Cmondesin.php" );
+?>

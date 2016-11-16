1ff5082b3b1c6fc9c81c099e3cd6975cb590ea98
kudotimu <kudotiko999@ezweb.ne.jp>
2016-11-16 10:49:49 +0900

    Cmon1hint.php

--- Cmon1hint.php
+++ Cmon1hint.php
@@ -1,11 +1,51 @@
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<html lang="ja">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap-material-design.css">
+<link rel="stylesheet" type="text/css" href="css/ripples.css">
+<link rel="stylesheet" type="text/css" href="css/styles.css">
+
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/material.min.js"></script>
+<script type="text/javascript" src="js/ripples.min.js"></script>
+
+<style>
+
+body{ 
+  background-color:#dddddd;
+}
+
+.center1{
+  text-align:center;
+  font-size:200%;
+  margin:60px;
+}
+
+.hint{
+  margin-left:20%;
+  margin-right:20%;
+  font-size:150%;
+  background-color:#ffffff;
+}
+
+</style>
 <title>問題１ヒント</title>
 </head>
-<body>
-<p>問題１ヒント</p>
 
-<p>変数を３つ宣言し、１つに３、1つに５を代入、もう一つの変数に左辺値として計算を代入する。</p>
+<body>
+<div class="center1">問題１ヒント</div>
+
+<div class="hint">
+＜考え方＞<br>
+①二つ変数を用意し、それぞれに数値を代入する。<br>
+②合計を入れるための変数を用意する。<br>
+③二つ足した値を②で用意した変数に代入する。<br>
+</div>
+
 </body>
 </html>
